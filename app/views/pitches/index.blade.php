e27e9d87c1acb58914664c0453243d0ab5843ddf
Ryan Bentkowski <ryanbentkowski@gmail.com>
2016-01-20 16:51:30 -0600

    Added connection pitches connection to navbar.  and started formating pitches/index.

--- app/views/pitches/index.blade.php
+++ app/views/pitches/index.blade.php
@@ -6,6 +6,14 @@
 
 @section('content')
 
+<div class="container">
+    <div class="row" style="background-color:papayawhip;">
+        <div class="col-md-4" style="background-color:burlywood;">hello</div>
+        <div class="col-lg-4"></div>
+    </div>
+    <div class="row"></div>
+</div>
+
 /vagrant/sites/hopshop.dev/app/views/pitches/index.blade.php
 
 @stop
