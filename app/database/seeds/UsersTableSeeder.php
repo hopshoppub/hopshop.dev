5f02591a2135abca19c8bf388c763e7e48a11185
Ryan Bentkowski <ryanbentkowski@gmail.com>
2016-01-20 14:42:59 -0600

    Just put back in user seeder data.

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -7,14 +7,15 @@ class UsersTableSeeder extends Seeder {
 
 	public function run()
 	{
-		// $faker = Faker::create();
-
-		// foreach(range(1, 10) as $index)
-		// {
-		// 	User::create([
-
-		// 	]);
-		// }
+		$user = new User();
+ 		$user->first_name = $_ENV['USER_FIRST_NAME'];
+ 		$user->last_name = $_ENV['USER_LAST_NAME'];
+ 		$user->user_name = $_ENV['USER_NAME'];
+ 		$user->email = $_ENV['USER_EMAIL'];
+ 		$user->password = Hash::make($_ENV['USER_PASS']);
+ 		$user->zip_code = $_ENV['USER_ZIP'];
+ 		$user->role = $_ENV['USER_ROLE'];
+ 		$user->save();
 	}
 	
 }
