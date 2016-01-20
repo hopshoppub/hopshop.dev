672a9a51f7ceeea629512179281bfb0a4008c1f5
Ryan Bentkowski <ryanbentkowski@gmail.com>
2016-01-20 14:22:26 -0600

    Made the pitches.show page.  Created seeder data.  Updated migration data.  Created pitchesmaster.

--- app/models/Pitch.php
+++ app/models/Pitch.php
@@ -2,51 +2,59 @@
 
 class Pitch extends BaseModel {
 
-	// Add your validation rules here
-	public static $rules = [
-		// 'title' => 'required'
-	];
+    // Add your validation rules here
+    public static $rules = [
+        // 'title' => 'required'
+    ];
 
-	// Don't forget to fill this array
-	protected $fillable = [];
+    // Don't forget to fill this array
+    protected $fillable = [];
 
-	/*
-	 *	Pitch belongsTo a user on User
-	 */
-	public function user()
-	{
-		return $this->belongsTo('User');
-	}
+    /*
+     *  Pitch belongsTo a creator on User
+     */
+    public function creator()
+    {
+        return $this->belongsTo('User');
+    }
 
-	/*
-	 *	Pitch hasMany contributions on Contribution
-	 */
-	public function contributions()
-	{
-		return $this->hasMany('Contribution');
-	}
+    /*
+     *  Pitch hasMany contributions on Contribution
+     */
+    public function contributions()
+    {
+        return $this->hasMany('Contribution');
+    }
 
-	/*
-	 *	Pitch hasMany contributors Through Contribution on User
-	 */
-	public function contributors()
-	{
-		return $this->hasManyThrough('User','Contribution');
-	}
+    /*
+     *  Pitch hasMany contributors Through Contribution on User
+     */
+    public function contributors()
+    {
+        return $this->hasManyThrough('User','Contribution');
+    }
 
-	/*
-	 *	Pitch belongsToMany watchers on User threw watches
-	 */
-	public function watchers()
-	{
-		return $this->belongsToMany('User', 'watches','pitch_id','user_id');
-	}
+    /*
+     *  Pitch belongsToMany watchers on User threw watches
+     */
+    public function watchers()
+    {
+        return $this->belongsToMany('User', 'watches','pitch_id','user_id');
+    }
 
-	/*
-	 *	Pitch hasMany updates on Update
-	 */
-	public function updates()
-	{
-		return $this->hasMany('Update');
-	}
+    /*
+     *  Pitch hasMany updates on Update
+     */
+    public function updates()
+    {
+        return $this->hasMany('Update');
+    }
+
+    /*
+     *  Pitch belongsTo brewery on Pitch
+     */ 
+    public function brewery()
+    {
+        return $this->belongsTo('Brewery');
+    }
 }
--- app/views/layouts/pitchesmaster.blade.php
+++ app/views/layouts/pitchesmaster.blade.php
@@ -1,2 +1,22 @@
 @extends('layouts.master')
 
+
+@section('top-script')
+
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	
+
+
+@stop
+
+
+@section('content')
+<div class="container">
+	<div class='row'>
+		<h2 id="title">Pitches</h2>
+	</div>
+</div>
+
+
+
+@stop
