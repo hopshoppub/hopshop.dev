711948ecd78350798ff79426fd3bf0eac396ceff
Vaughn <alvaradovaughn@yahoo.com>
2016-01-21 16:11:15 -0600

    css files

--- app/views/breweries/index.blade.php
+++ app/views/breweries/index.blade.php
@@ -14,22 +14,22 @@
             <div class="col-xs-8 col-md-3"><img src="http://placehold.it/350x150" alt="Mountain View" style="width:250px;height:200px;"></div>
             <div class="col-xs-5 col-md-3"><h4>Beer Name: </h4> <h3>{{{ $breweries->beer->name }}}</h3></div>
             <div class="col-xs-5 col-md-3"><h4>Rating:</h4>
-               {{--  {{ Form::open(array('action' => 'RatingsController@store')) }} --}}
-                    <div class="stars">
-                        <input type="radio" name="star" class="star-1" id="star-1" value='1'/>
-                        <label class="star-1" for="star-1">1</label>
-                        <input type="radio" name="star" class="star-2" id="star-2" value='2'/>
-                        <label class="star-2" for="star-2">2</label>
-                        <input type="radio" name="star" class="star-3" id="star-3" value='3'/>
-                        <label class="star-3" for="star-3">3</label>
-                        <input type="radio" name="star" class="star-4" id="star-4" value='4'/>
-                        <label class="star-4" for="star-4">4</label>
-                        <input type="radio" name="star" class="star-5" id="star-5" value'5'/>
-                        <label class="star-5" for="star-5">5</label>
-                        <span></span>
-                    </div>
-                  <button class="btn btn-primary btn-block" id='margin-button'type="submit">Submit Your Rating!</button>
-               {{--  {{Form::close()}} --}}
+               <form id="ratingsForm">
+					<div class="stars">
+						<input type="radio" name="star" class="star-1" id="star-1" />
+						<label class="star-1" for="star-1">1</label>
+						<input type="radio" name="star" class="star-2" id="star-2" />
+						<label class="star-2" for="star-2">2</label>
+						<input type="radio" name="star" class="star-3" id="star-3" />
+						<label class="star-3" for="star-3">3</label>
+						<input type="radio" name="star" class="star-4" id="star-4" />
+						<label class="star-4" for="star-4">4</label>
+						<input type="radio" name="star" class="star-5" id="star-5" />
+						<label class="star-5" for="star-5">5</label>
+						<span></span>
+					</div>
+				  
+				</form>
             </div>
         </div>
         <hr>
@@ -38,7 +38,12 @@
             <div class="col-xs-8 col-md-3"><h4>ABV: </h4> <h3>{{{ $breweries->beer->abv }}}</h3></div>
             <div class="col-xs-5 col-md-3"><h4>Style: </h4> <h3>{{{ $breweries->beer->style->style_name }}}</h3></div>
         </div>
-    <a href="#" class='pull-right' id='description-link'>Description: </a>
+        <div class='row'>
+	        <div class="description-toggle" data-beer-id="{{{ $beers->beer_id }}}">
+	        	<div class="col-xs-8 col-md-12"><h4> </h4> <h3>{{{ $beer->description }}}</h3></div>
+	        </div>
+	    </div>
+    	<button class='pull-right description-button' data-grab-id="{{{ $beers->beer_id }}}">Click Here For Description: </button>
     </div>
 </div>
 @endforeach
