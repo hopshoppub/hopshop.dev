63bd882c67d177dd40d42642e6a622f8f9d9fa42
Ryan Bentkowski <ryanbentkowski@gmail.com>
2016-01-20 18:36:32 -0600

    Made pitches index page spit out info from db including video.  added some css.

--- app/views/pitches/index.blade.php
+++ app/views/pitches/index.blade.php
@@ -6,15 +6,24 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row" style="background-color:papayawhip;">
-        <div class="col-md-4" style="background-color:burlywood;">hello</div>
-        <div class="col-lg-4"></div>
-    </div>
-    <div class="row"></div>
+<div class="beerpitches">
+	<div class="container">
+		<div class="row" style="background-color:papayawhip;">
+		@foreach($pitches as $pitch)
+		<div class="pitchesindex">
+	        <div class="col-sm-12 col-sm-6 col-md-4 col-lg-4" style="background-color:burlywood;">
+	        	<h4>{{{ $pitch->title }}} by<br> {{{ $pitch->brewery->name }}}(${{{ $pitch->goal }}})</h4>
+	            <div><iframe width="300" height="169" src="{{{ $pitch->video }}}"frameborder="0" allowfullscreen></iframe></div>
+	        </div>
+	    </div>
+		@endforeach
+		</div>
+	</div>
 </div>
 
-/vagrant/sites/hopshop.dev/app/views/pitches/index.blade.php
+ 
+
+
 
 @stop
 
