40144fe23cde9678461b2db54e5a50201deb1017
shawn pivonka <sprov03@gmail.com>
2016-01-22 10:27:54 -0600

    Added route to home controller Created a button partial for facebook sharing Modified a little css to get facbook button working

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -21,22 +21,11 @@ $(document).ready(function() {
 
 	    	user: {
 
-		        // email: '',
-		        // password: '',
 	    	},
 
 	    	loginPassword: '',
 	    	loginEmail: '',
 
-
-	        // first_name:'',
-	        // last_name:'',
-	        // email:'',
-	        // confirm_email:'',
-	        // password:'',
-	        // confirm_password:'',
-	        // zip_code:'',
-	        // user_name:'',
 
 	    },
 
@@ -44,33 +33,16 @@ $(document).ready(function() {
 
 	        loginClicked: function() {
 
-	            var loginInfo  = {
-	                email : this.loginEmail,
-	                password  : this.loginPassword,
-	            };
-
-            // login(loginInfo);
-            login(testVue.$data.user);
+            	login( testVue.$data.user );
         	},
 
         	signupClicked: function() {
 
-        		var signupInfo = {
-
-        			first_name : this.signupFirstName,
-	        		last_name : this.signupLastName,
-	        		user_name : this.signupUserName,
-	        		email : this.signupEmail,
-	        		// signupConfirmEmail:'',
-	        		password : this.signupPassword,
-	        		// signupConfirmPassword:'',
-	        		zip_code : this.signupZipCode,
-        		};
-        		// signup( signupInfo );
         		signup ( testVue.$data.user );	
         	},
 
         	logoutClicked: function() {
+
         		logout();
         	}
 	    }
@@ -84,7 +56,7 @@ $(document).ready(function() {
 
 		    testVue.$data.user = data;
 
-	    }).error(function (data, status, request) {
+	    }).catch(function (data, status, request) {
 	        alert('sorry an error accored and you were not logged out');
 	    }); 
 	}
@@ -100,19 +72,127 @@ $(document).ready(function() {
 	        	testVue.$data.user = data;
 
 		        testVue.$data.loggedIn = true;
+
+		        $('#login_modal').modal('toggle');
 	        }
-	    }).error(function (data, status, request) {
+	    }).catch(function (data, status, request) {
 	        console.log("error");
 	    }); 
 	}
 
 	function signup(obj) {
-		console.log('pressed');
+
 	    Vue.http.post('/users/store', obj, function (data, status, request) {
-	        // tableVue.$data.contacts.push(data);
-	        console.log(data);
-	    }).error(function (data, status, request) {
+			testVue.$data.user = data;
+
+			$('#signup_modal').modal('toggle');	        
+	    }).catch(function (data, status, request) {
 	        alert("error");
 	    }); 
 	}
+
+	function facebookLogin(id) {
+
+            console.log( 'tsting it' + id );
+	    Vue.http.post('/facebookLogin/' + id , function (data, status, request) {
+			
+	    	testVue.$data.loggedIn = true;
+
+	    	testVue.$data.user = data;
+
+	    	// console.log(data + 'form fb login');
+	    	// console.log('fb_id: ' + testVue.$data.user.facebook_id);
+	    	console.log(data);
+        
+	    }).catch(function (data, status, request) {
+	        alert("error");
+	    });
+	}
+
+
+ // This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response.authResponse);
+        FB.api('/me', function(me){
+        if (me.id) {
+            // var facebook_userid = me.id;
+            // alert(facebook_userid);
+            testVue.$data.user.facebook_id = me.id;
+	      // console.log( 'new test' + testVue.$data.user.facebook_id );
+
+	    if (response.status === 'connected') {
+	      // Logged into your app and Facebook.
+	      // testAPI();
+	      facebookLogin( testVue.$data.user.facebook_id );
+	    } else if (response.status === 'not_authorized') {
+	      // The person is logged into Facebook, but not your app.
+	      document.getElementById('status').innerHTML = 'Please log ' +
+	        'into this app.';
+	    } else {
+	      // The person is not logged into Facebook, so we're not sure if
+	      // they are logged into this app or not.
+	      document.getElementById('status').innerHTML = 'Please log ' +
+	        'into Facebook.';
+	    }
+        }
+    });
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1670397703235376',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.2' // use version 2.2
+  });
+
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+
 });
