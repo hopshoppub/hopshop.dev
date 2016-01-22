982bab7078f830691998434334943adec6f665e0
shawn pivonka <sprov03@gmail.com>
2016-01-22 10:21:57 -0600

    added a Route for facebookLogin

--- app/routes.php
+++ app/routes.php
@@ -14,6 +14,7 @@
 /*
  *		Home Routes
  */
+Route::post('/facebookLogin/{fb_id}', 'HomeController@fbLogin');
 Route::post('/login', 'HomeController@postLogin');
 Route::get('/logout', 'HomeController@getLogout');
 Route::get('/', 'HomeController@showHome');
