1edd5d9cf91d1d567e09a8565ededbbbdb4afe9c
Ryan Bentkowski <ryanbentkowski@gmail.com>
2016-01-22 17:22:54 -0600

    Added link from pitches index to show/$id page.

--- app/views/pitches/index.blade.php
+++ app/views/pitches/index.blade.php
@@ -11,10 +11,11 @@
 		<div class="row" style="background-color:papayawhip;">
 		@foreach($pitches as $pitch)
 		<div class="pitchesindex">
+			<a href="{{{ action('PitchesController@show', $pitch->pitch_id) }}}">
 	        <div class="col-sm-12 col-sm-6 col-md-4 col-lg-4" style="background-color:burlywood;">
 	        	<h4>{{{ $pitch->title }}} by<br> {{{ $pitch->brewery->name }}}(${{{ $pitch->goal }}})</h4>
 	            <div><iframe width="300" height="169" src="{{{ $pitch->video }}}"frameborder="0" allowfullscreen></iframe></div>
-	        </div>
+	        </div></a>
 	    </div>
 		@endforeach
 		</div>
