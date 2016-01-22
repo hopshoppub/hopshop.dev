a9ae57c374134ea65edbb31c5ce9f7b3c3842ed5
Vaughn <alvaradovaughn@yahoo.com>
2016-01-21 19:13:25 -0600

    js to find out metadata

--- app/views/beers/index.blade.php
+++ app/views/beers/index.blade.php
@@ -22,22 +22,21 @@
             <div class="col-xs-8 col-md-3"><img src="http://placehold.it/350x150" alt="Mountain View" style="width:250px;height:200px;"></div>
             <div class="col-xs-5 col-md-3"><h4>Beer Name: </h4> <h3>{{{ $beer->name }}}</h3></div>
             <div class="col-xs-5 col-md-3"><h4>Rating:</h4>
-                <form id="ratingsForm">
-                    <div class="stars">
-                        <input type="radio" name="star" class="star-1" id="star-1" />
+                <div>
+                    <div class="stars" data-star-id="{{{ $beer->beer_id }}}">
+                        <input type="radio" name="star" class="star-1" id="star-1" value='1'/>
                         <label class="star-1" for="star-1">1</label>
-                        <input type="radio" name="star" class="star-2" id="star-2" />
+                        <input type="radio" name="star" class="star-2" id="star-2" value='2'/>
                         <label class="star-2" for="star-2">2</label>
-                        <input type="radio" name="star" class="star-3" id="star-3" />
+                        <input type="radio" name="star" class="star-3" id="star-3" value='3'/>
                         <label class="star-3" for="star-3">3</label>
-                        <input type="radio" name="star" class="star-4" id="star-4" />
+                        <input type="radio" name="star" class="star-4" id="star-4" value='4'/>
                         <label class="star-4" for="star-4">4</label>
-                        <input type="radio" name="star" class="star-5" id="star-5" />
+                        <input type="radio" name="star" class="star-5" id="star-5" value='5'/>
                         <label class="star-5" for="star-5">5</label>
                         <span></span>
                     </div>
-                  
-                </form>
+               </div>
             </div>
         </div>
         <hr>
@@ -53,11 +52,9 @@
             </div>
         </div>
         <button class='pull-right description-button btn' data-grab-id="{{{ $beer->beer_id }}}">Click Here For Description: </button>
-    {{-- <a href='#' class='pull-right' id='description-link'>Description: </a> --}}
 </div>
 </div>
 @endforeach
-{{-- {{  (Input::has('search')) ? null: $beers->links() }} --}}
         
 @stop
 
