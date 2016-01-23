319477c546ada9a4d5d697aa255fd676691f40e7
Ryan Bentkowski <ryanbentkowski@gmail.com>
2016-01-22 18:14:07 -0600

    Added CSS and divs around pitches.

--- app/views/pitches/index.blade.php
+++ app/views/pitches/index.blade.php
@@ -3,7 +3,7 @@
 @section('top-script')
 
 @stop
-
+ <link rel="stylesheet" href="/css/pitches.css">
 @section('content')
 
 <div class="beerpitches">
@@ -12,9 +12,11 @@
 		@foreach($pitches as $pitch)
 		<div class="pitchesindex">
 			<a href="{{{ action('PitchesController@show', $pitch->pitch_id) }}}">
-	        <div class="col-sm-12 col-sm-6 col-md-4 col-lg-4" style="background-color:burlywood;">
-	        	<h4>{{{ $pitch->title }}} by<br> {{{ $pitch->brewery->name }}}(${{{ $pitch->goal }}})</h4>
-	            <div><iframe width="300" height="169" src="{{{ $pitch->video }}}"frameborder="0" allowfullscreen></iframe></div>
+	        <div class="col-sm-12 col-sm-6 col-md-4 col-lg-4" {{-- style="background-color:burlywood;" --}}>
+	        	<div class='pitches-table'>
+		        	<h4>{{{ $pitch->title }}} by<br> {{{ $pitch->brewery->name }}}(${{{ $pitch->goal }}})</h4>
+		            <div><iframe width="300" height="169" src="{{{ $pitch->video }}}"frameborder="0" allowfullscreen></iframe></div>
+	        	</div>
 	        </div></a>
 	    </div>
 		@endforeach
