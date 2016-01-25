e40b9091cec7bdb5996b9b1ccadb3f2c48670b18
shawn pivonka <sprov03@gmail.com>
2016-01-25 13:40:15 -0600

    added fake seeer rating for beer nine

--- app/database/seeds/RatingsTableSeeder.php
+++ app/database/seeds/RatingsTableSeeder.php
@@ -14,16 +14,63 @@ class RatingsTableSeeder extends Seeder {
 		$rating->comment = 'Great Beer!';
 		$rating->save();
 
+		$rating = new Rating();
+		$rating->user_id = 2;
+		$rating->beer_id = 9;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
 
-    	// $faker = Faker::create();
-    	// foreach (range(1,1000) as $index) {
-	    //     DB::table('users')->insert([
-	    //     	'user_id' => $faker->numberBetween(1,100),
-	    //         'beer_id' => $faker->numberBetween(10,200),
-	    //         'rating' => $faker->numberBetween(1,5),
-	    //         'comment' => $faker->paragraph,
-	    //     ]);
-     //    }
+		$rating = new Rating();
+		$rating->user_id = 3;
+		$rating->beer_id = 9;
+		$rating->rating = '4';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 4;
+		$rating->beer_id = 9;
+		$rating->rating = '1';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 5;
+		$rating->beer_id = 9;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 6;
+		$rating->beer_id = 9;
+		$rating->rating = '4';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 7;
+		$rating->beer_id = 9;
+		$rating->rating = '1';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 8;
+		$rating->beer_id = 9;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 9;
+		$rating->beer_id = 9;
+		$rating->rating = '4';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+
 	}
 
 }
