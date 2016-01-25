55d28b4dac5234daea80adb44bc8fb9dd915b1a4
Vaughn <alvaradovaughn@yahoo.com>
2016-01-25 13:40:45 -0600

    added seeder for this

--- app/database/seeds/RatingsTableSeeder.php
+++ app/database/seeds/RatingsTableSeeder.php
@@ -8,22 +8,69 @@ class RatingsTableSeeder extends Seeder {
 	public function run()
 	{
 		$rating = new Rating();
-		$rating->user_id = 1;
+		$rating->user_id =10;
 		$rating->beer_id = 9;
 		$rating->rating = '5';
 		$rating->comment = 'Great Beer!';
 		$rating->save();
 
+		$rating = new Rating();
+		$rating->user_id = 11;
+		$rating->beer_id = 9;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
 
-    	// $faker = Faker::create();
-    	// foreach (range(1,1000) as $index) {
-	    //     DB::table('users')->insert([
-	    //     	'user_id' => $faker->numberBetween(1,100),
-	    //         'beer_id' => $faker->numberBetween(10,200),
-	    //         'rating' => $faker->numberBetween(1,5),
-	    //         'comment' => $faker->paragraph,
-	    //     ]);
-     //    }
+		$rating = new Rating();
+		$rating->user_id = 12;
+		$rating->beer_id = 9;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 13;
+		$rating->beer_id = 9;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 14;
+		$rating->beer_id = 9;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 15;
+		$rating->beer_id = 9;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 16;
+		$rating->beer_id = 9;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 17;
+		$rating->beer_id = 11;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->user_id = 18;
+		$rating->beer_id = 11;
+		$rating->rating = '5';
+		$rating->comment = 'Great Beer!';
+		$rating->save();
+
+    	
 	}
 
 }
