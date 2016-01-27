ca13d72053f2ea536dcbd670af9f0fd44936e518
Vaughn <alvaradovaughn@yahoo.com>
2016-01-27 13:00:33 -0600

    testing for infinite scroll  # Please enter the commit message for your changes. Lines starting

--- public/js/test.js
+++ public/js/test.js
@@ -4,11 +4,33 @@
 		// if( myVue.$data.infinateFlag !== false )
 		// {
 		    if($(window).scrollTop() + $(window).height() == $(document).height() ) {
-		        myVue.test();
+		    	infiniteScroll();
+
 		    }
 			// myVue.$data.infinateFlag = false;
 		// }
 	});
+
+	function infiniteScroll() {
+
+		var offset = myVue.$data.beers.length;
+		console.log(offset);
+	        $.get("/beerpage/" + offset, function(data) {
+                data.forEach(function(element) {
+                    myVue.$data.beers.push(element);
+                    showBeerCalculatedRating();
+                    showUserRatingInput();
+
+                });
+
+            });
+
+	}
+
+
+	infiniteScroll();
+
+
 // }
 
 // module.exports = run;
