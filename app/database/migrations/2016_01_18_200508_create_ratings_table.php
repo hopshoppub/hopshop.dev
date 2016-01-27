42c8cd4645915cf4c5a047a9ba25167b287f6164
Vaughn <alvaradovaughn@yahoo.com>
2016-01-27 10:32:31 -0600

    giving ratings primary key so i can update ratings

--- app/database/migrations/2016_01_18_200508_create_ratings_table.php
+++ app/database/migrations/2016_01_18_200508_create_ratings_table.php
@@ -14,13 +14,15 @@ class CreateRatingsTable extends Migration {
 	{
 		Schema::create('ratings', function(Blueprint $table)
 		{
+			$table->increments('rating_id');
+
 			$table->integer('user_id')->unsigned();
 			$table->foreign('user_id')->references('user_id')->on('users');
 
 			$table->integer('beer_id')->unsigned();
 			$table->foreign('beer_id')->references('beer_id')->on('beers');
 
-			$table->primary(['beer_id', 'user_id']);
+			$table->unique(['beer_id', 'user_id']);
 
 			$table->string('rating', 1);
 			$table->string('comment',300);
